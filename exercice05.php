83420789bcf35f116f127f7a0206a7a9c4f8538b
Thierry <thierry@beweb-16.176R>
2018-04-26 10:53:03 +0200

    rajout de l'exercice 05 terminer

---
+++ exercice05.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thierry
+ * Date: 26/04/18
+ * Time: 10:23
+ */
+
+$compteur = 10;
+
+
+while($compteur <=100){
+
+
+
+    echo("--".$compteur ."--");
+
+    $compteur++;
+
+
+}
