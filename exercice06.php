2eeb420895422f395b7849331d6f22321d4541d8
Thierry <thierry@beweb-16.176R>
2018-04-26 13:53:06 +0200

    rajout de la deuxieme partie de l'exo 6

--- exercice06.php
+++ exercice06.php
@@ -1,4 +1,5 @@
 <?php
+//---------------------Premiere partie--------------------------------------------------------------------
 
 //initialisation d'un tableau a vide
 $tableau = [];
@@ -8,12 +9,30 @@ $i = 0;
 while($i < 10){
 
 //j'insere des boolean vrai à chaque itération
-$tableau[$i] = true;
+$tableau[$i] = "tata";
 
 
-echo($tableau[$i]. " index : ". $i . " ;");
+echo $tableau[$i]. " index : ". $i . " ;";
 $i++;
 }
 
+//----------------------deuxieme partie----------------------------------------------------------------------
 
-?>
+
+
+
+
+$i = 0;
+
+//ma longueur du tableau est de 10,donc je boucle 10 fois
+while($i < 10){
+
+//j'insere des boolean vrai à chaque itération
+    $tableau[$i] = "toto";
+
+
+    echo $tableau[$i]. " index : ". $i . " ;";
+    $i++;
+}
+
+var_dump($tableau);
