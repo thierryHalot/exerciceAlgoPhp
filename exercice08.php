8d60c61c9b1e3336e973c90558972fb9a903a021
Thierry <thierry@beweb-16.176R>
2018-04-29 14:02:57 +0200

    ajout de l'exercice 8 finalisé

---
+++ exercice08.php
@@ -0,0 +1,10 @@
+<?php
+
+$variableEntiere = 243;
+
+//tant que ma variable est supérieur a 0,je la décrémente
+while($variableEntiere > 0){
+
+    $variableEntiere--;
+    echo $variableEntiere."\n";
+}
